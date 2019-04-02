fb4f8aaf52879fdeeea798929e9b3620af1b2552
genek771 <genek771@yandex.ru>
2019-04-02 22:55:41 +0300

    Flask migrations и регистрация пользователя

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager, current_user, login_required
 from webapp.user.views import blueprint as user_blueprint
 from webapp.admin.views import blueprint as admin_blueprint
 from webapp.news.views import blueprint as news_blueprint
+from flask_migrate import Migrate
 
 from webapp.db import db
 from webapp.user.model import User
@@ -13,6 +14,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
