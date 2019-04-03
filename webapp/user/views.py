9ce27adb9d75304aecd256d2fe7186c873e8960e
genek771 <genek771@yandex.ru>
2019-04-03 15:23:46 +0300

    Added news HabraHabr and Celery support

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -56,5 +56,13 @@ def process_register():
         db.session.commit()
         flash('Вы успешно зарегистрировались!')
         return redirect(url_for('user.login'))
-    flash('Пожалуйста, исправьте ошибки в форме')
-    return redirect(url_for('user.register'))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в поле "{}": - {}'.format(
+                    getattr(form, field).label.text,
+                    error
+                ))
+
+
+        return redirect(url_for('user.register'))
